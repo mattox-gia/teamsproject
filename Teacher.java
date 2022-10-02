64cd5c47de890eb74d84ac6e6d6414f4da7f49e0
Gia Mattox <gmattox26@gmail.com>
2022-10-02 18:43:32 -0500

    commit

--- Teacher.java
+++ Teacher.java
@@ -1,13 +1,5 @@
 import java.io.Serializable;
 
 public class Teacher implements Serializable {
-//     static string subject;
-//     static string teacher;
-//     static int grade;
-//     static boolean current;
 
-// public Teacher(subject,teacher,grade,current){
-//     this.subject = subject;
-
-//}
 }
